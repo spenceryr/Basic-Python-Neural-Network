a6efe0a076553ce0a3710586a066cd80769b9e25
Spencer Reitman Laptop <spencerreitman@gmail.com>
2019-10-09 17:31:23 -0700

    iuno bruh. It looks like it works but increasing the learning rate only makes the error go down, which it probably shouldn't up to a certain point

--- first_test.py
+++ first_test.py
@@ -6,9 +6,11 @@ import numpy as np
 train_set = []
 holdout_set = []
 test_set = []
-pca_k = 50
-learning_rate = 0.01
+learning_rate = .01
+k_pca = 8
 epochs = 10
+c1 = 'ht'
+c2 = 'm'
 
 def logistic_reg(w, x):
     return 1/(1+np.power(np.e, -1 * (w.T.dot(x[0]))))
@@ -21,6 +23,11 @@ def gradient(t, x, w):
     """
     return (t - logistic_reg(w, x)) * x[0]
 
+def calc_error(w):
+    return (-1/len(holdout_set)) * sum([(((1 if c1 in label else 0) *
+                                    np.log(logistic_reg(w, x))) +
+                                    ((1-(1 if c1 in label else 0)) * np.log(1-logistic_reg(w, x)))) for x, label in holdout_set])
+
 def batch_gradient_descent(epochs, learning_rate, dim_input):
     """
     param: epochs: number of trials to run
@@ -28,16 +35,13 @@ def batch_gradient_descent(epochs, learning_rate, dim_input):
     param: dim_input: dimension of input
     """
     w = np.zeros(dim_input)
+    current_error = calc_error(w)
     for _ in range(epochs):
-        v = w + (learning_rate * (1/len(train_set)) * sum([gradient(1 if 'ht' in label else 0,image,w) for image, label in train_set]))
-        testv = 0
-        testw = 0
-        for image, label in holdout_set:
-              log_reg_v = logistic_reg(v, image)
-              testv += 1 if (log_reg_v > .5 and 'ht' in label) or (log_reg_v <= .5 and 'm' in label) else 0
-              log_reg_w = logistic_reg(w, image)
-              testw += 1 if (log_reg_w > .5 and 'ht' in label) or (log_reg_w <= .5 and 'm' in label) else 0
-        w = v if testv > testw else w
+        r = (learning_rate * sum([gradient(1 if c1 in label else 0,image,w) for image, label in train_set]))
+        print(r)
+        v = w + r
+        new_error = calc_error(v)
+        w = v if new_error < current_error else w
     return w
 
 def main():
@@ -46,32 +50,29 @@ def main():
     global test_set
     images, labels = load_data(data_dir="./CAFE/")
     data = zip(images,labels)
-    ht_m_data = [(image, label) for image, label in data if 'm' in label.strip('pgm') or 'ht' in label.strip('pgm')]
-    for _ in range(int(len(ht_m_data) * .6)):
-        random_index = rand.randint(0, len(ht_m_data)-1)
-        train_set.append(ht_m_data[random_index])
-        del ht_m_data[random_index]
-    for _ in range(int(len(ht_m_data) * .5)):
-        random_index = rand.randint(0, len(ht_m_data)-1)
-        holdout_set.append(ht_m_data[random_index])
-        del ht_m_data[random_index]
-    test_set = ht_m_data
+    slice_data = [(image, label) for image, label in data if c2 in label.strip('pgm') or c1 in label.strip('pgm')]
+    for i in range(5):
+        test_starting_index = (i * 4)
+        test_ending_index = test_starting_index + 4
+        test_set = slice_data[test_starting_index:test_ending_index]
+        remaining = slice_data[:test_starting_index] + slice_data[test_ending_index:]
+        rand_index = rand.randint(0, len(remaining)-1)
+        holdout_set = [remaining[::2][rand_index//2]] + [remaining[1::2][rand_index//2]]
+        del remaining[(rand_index if rand_index % 2 == 0 else rand_index-1):(rand_index+2 if rand_index % 2 == 0 else rand_index+1)]
+        rand_index = rand.randint(0, len(remaining)-1)
+        holdout_set += [remaining[::2][rand_index//2]] + [remaining[1::2][rand_index//2]]
+        del remaining[(rand_index if rand_index % 2 == 0 else rand_index-1):(rand_index+2 if rand_index % 2 == 0 else rand_index+1)]
+        train_set = remaining
 
-    p_c_a = pca.PCA(len(train_set)-1)
-    p_c_a.fit(np.array([image for image,_ in train_set]))
-    train_set = [(p_c_a.transform(np.array(image)), label) for image, label in train_set]
-    holdout_set = [(p_c_a.transform(np.array(image)), label) for image, label in holdout_set]
-    test_set = [(p_c_a.transform(np.array(image)), label) for image, label in test_set]
+        p_c_a = pca.PCA(k_pca)
+        p_c_a.fit(np.array([image for image,_ in train_set]))
+        train_set = [(p_c_a.transform(np.array(image)), label) for image, label in train_set]
+        holdout_set = [(p_c_a.transform(np.array(image)), label) for image, label in holdout_set]
+        test_set = [(p_c_a.transform(np.array(image)), label) for image, label in test_set]
 
-    w = batch_gradient_descent(epochs, learning_rate, p_c_a.k)
-    total = 0
-    for image,label in (test_set + holdout_set):
-        output = logistic_reg(w, image)
-        print(output)
-        print(label)
-        total += 1 if (output > .5 and 'ht' in label) or (output <= .5 and 'm' in label) else 0
-    print(total/len(test_set + holdout_set))
-        
+        w = batch_gradient_descent(epochs, learning_rate, p_c_a.k)
+        print(calc_error(w))
+
 
 if __name__ == '__main__':
     main()
